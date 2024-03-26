4627b61719ebfa4576c8185ed92416cca56d4b43
Wilson Lin <code@wilsonl.in>
2024-03-26 20:52:49 +1100

    Implement deep detection and merging

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,77 @@
 
 import fs from "fs";
 import prettier from "prettier";
+import defined from "@xtjs/lib/js/defined";
+import UnreachableError from "@xtjs/lib/js/UnreachableError";
 
 const tsProp = (raw: string) =>
-  !/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(raw) ? `["${raw}"]` : raw;
+  !/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(raw) ? JSON.stringify(raw) : raw;
 
-export const generateTypeDefinition = (val: unknown): string => {
-  switch (typeof val) {
+type Simple =
+  | "bigint"
+  | "boolean"
+  | "function"
+  | "null"
+  | "number"
+  | "string"
+  | "symbol"
+  | "undefined";
+
+type ObjectMembers = Record<string, Type>;
+
+type Type = {
+  simples?: Set<Simple>;
+  object?: ObjectMembers;
+  instanceOf?: Set<string>;
+  array?: Type;
+};
+
+const mergeObjectMembers = (
+  a: ObjectMembers | undefined,
+  b: ObjectMembers | undefined
+): ObjectMembers | undefined => {
+  if (!a) {
+    return b;
+  }
+  if (!b) {
+    return a;
+  }
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  const merged: Record<string, Type> = {};
+  for (const prop of new Set([...aKeys, ...bKeys])) {
+    const m = mergeTypes(a[prop], b[prop]);
+    if (m) {
+      merged[prop] = m;
+    }
+  }
+  return merged;
+};
+
+const mergeTypes = (
+  a: Type | undefined,
+  b: Type | undefined
+): Type | undefined => {
+  if (!a) {
+    return b;
+  }
+  if (!b) {
+    return a;
+  }
+  return {
+    array: mergeTypes(a.array, b.array),
+    instanceOf: new Set([...(a.instanceOf ?? []), ...(b.instanceOf ?? [])]),
+    object: mergeObjectMembers(a.object, b.object),
+    simples: new Set([...(a.simples ?? []), ...(b.simples ?? [])]),
+  };
+};
+
+export const determineType = (
+  val: unknown,
+  arrayStrategy: "first" | "first+last" | "first+mid+last" | "all" = "all"
+): Type => {
+  const t = typeof val;
+  switch (t) {
     case "bigint":
     case "boolean":
     case "function":
@@ -15,32 +80,54 @@ export const generateTypeDefinition = (val: unknown): string => {
     case "string":
     case "symbol":
     case "undefined":
-      return typeof val;
+      return { simples: new Set([t]) };
     case "object":
       if (val === null) {
-        return "null";
+        return { simples: new Set(["null"]) };
       }
       if (Array.isArray(val)) {
-        if (val.length === 0) {
-          console.warn(`Found empty array`);
-          return `Array<unknown>`;
+        let candidates;
+        switch (arrayStrategy) {
+          case "first":
+            candidates = arrayStrategy.slice(0, 1);
+            break;
+          case "first+last":
+            candidates = [
+              ...arrayStrategy.slice(0, 1),
+              ...arrayStrategy.slice(-1),
+            ];
+            break;
+          case "first+mid+last":
+            candidates = [
+              arrayStrategy.at(0),
+              arrayStrategy.at(Math.floor(val.length / 2)),
+              arrayStrategy.at(-1),
+            ].filter(defined);
+            break;
+          case "all":
+            candidates = val;
+            break;
+          default:
+            throw new UnreachableError();
         }
-        return `Array<${generateTypeDefinition(val[0])}>`;
+        let elements: Type = {};
+        for (const v of candidates) {
+          elements = mergeTypes(elements, determineType(v, arrayStrategy))!;
+        }
+        return { array: elements };
       }
       const proto = Object.getPrototypeOf(val);
       switch (proto) {
         case Object.prototype:
         case null:
-          return [
-            "{",
-            ...Object.entries(val).map(
-              ([prop, val]) =>
-                `${tsProp(prop)}: ${generateTypeDefinition(val)};`
-            ),
-            "}",
-          ].join("\n");
+          const members = Object.fromEntries(
+            Object.entries(val!).map(
+              ([prop, val]) => [prop, determineType(val)] as const
+            )
+          );
+          return { object: members };
         default:
-          return proto.constructor.name;
+          return { instanceOf: new Set([proto.constructor.name]) };
       }
     // Fall through.
     default:
@@ -50,11 +137,36 @@ export const generateTypeDefinition = (val: unknown): string => {
   }
 };
 
+export const generateTypeDefinition = (t: Type): string => {
+  const parts = Array<string>();
+  if (t.array) {
+    parts.push(`Array<${generateTypeDefinition(t.array)}>`);
+  }
+  for (const s of t.instanceOf ?? []) {
+    parts.push(s);
+  }
+  for (const s of t.simples ?? []) {
+    parts.push(s);
+  }
+  if (t.object) {
+    parts.push(
+      [
+        "{",
+        ...Object.entries(t.object).map(
+          ([prop, val]) => `${tsProp(prop)}: ${generateTypeDefinition(val)};`
+        ),
+        "}",
+      ].join("")
+    );
+  }
+  return parts.join(" | ") || "unknown";
+};
+
 if (require.main == module) {
   console.log(
     prettier.format(
       `type MyCustomType = ${generateTypeDefinition(
-        JSON.parse(fs.readFileSync(0, "utf-8"))
+        determineType(JSON.parse(fs.readFileSync(0, "utf-8")))
       )}`,
       { parser: "typescript" }
     )
